79d53647707db816da4066e35e33d79d2f61a215
joxerx <n.lebedinsky@bk.ru>
2022-12-15 23:07:43 +0300

    Добавлена функция вычисления количества покупок для снижения средней стоимости

--- reducer-console.py
+++ reducer-console.py
@@ -36,13 +36,38 @@ def getinteger(msg):
             continue
 
 
+def decrease_average_cost(cur_price, wanted_percent, deals_count):
+    spent = 0
+    quantity = 0
+    for i in range(deals_count):
+        spent += purchase_price[i] * purchases_amount[i]
+        quantity += purchases_amount[i]
+
+    start_average_price = spent/quantity
+    average_price = start_average_price
+
+    print("Начальная средняя стоимость: " + str(start_average_price))
+
+    new_quantity = 0
+    while average_price > start_average_price * (1 - (wanted_percent / 100)):
+        spent += cur_price
+        quantity += 1
+        average_price = spent/quantity
+        new_quantity += 1
+        if average_price > start_average_price:
+            res = "При указанной текущей цене невозможно снизить среднюю стоимость покупки"
+            return res
+    res = "Чтобы добиться желаемого результата необходимо приобрести " + str(new_quantity) + " единиц по текущей стоимости"
+    return res
+
+
 print("Нужно ввести информацию обо всех сделках по интересуемому активу. Чтобы прекратить ввод, отправьте 0.")
 purchase_price = []
 purchases_amount = []
-deals_counter = 1
+deals_counter = 0
 while True:
     while True:
-        price = getfloat("Цена покупки №" + str(deals_counter) + ": ")
+        price = getfloat("Цена покупки №" + str(deals_counter + 1) + ": ")
         if price > 0:
             purchase_price.append(price)
             break
@@ -54,7 +79,7 @@ while True:
     if len(purchase_price) == len(purchases_amount):
         break
     while True:
-        amount = getinteger("Количество №" + str(deals_counter) + ": ")
+        amount = getinteger("Количество №" + str(deals_counter + 1) + ": ")
         if amount > 0:
             purchases_amount.append(amount)
             deals_counter += 1
@@ -70,3 +95,5 @@ while True:
 
 current_price = getfloat("Текущая стоимость: ")
 decrease_percent = getfloat("На сколько процентов нужно снизить среднюю стоимость: ")
+print(decrease_average_cost(current_price, decrease_percent, deals_counter))
+
